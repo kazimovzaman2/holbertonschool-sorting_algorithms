491206d14952b1eabf88f9dcfef1bd4e91337d7c
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-16 13:25:10 +0400

    Initialize 2-selection_sort.c & 2-O files for task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+
+/**
+ * selection_sort - sorts a doubly linked list
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+    
+}
